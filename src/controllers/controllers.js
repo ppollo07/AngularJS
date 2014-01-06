f41e41989384b33ffe523da29d8efa35cfe34acf
ppollo07 <4526553>
2014-01-06 18:03:31 -0300

    api nueva
    una nueva api a ver que pasa
    http://mymovieapi.com/

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -2,15 +2,16 @@
 
 movieApp.controller("homeControler", function($scope, movieService){
 	$scope.id = "tt1170358";
+	$scope.title = "home";
 	$scope.xhrData = {};
     $scope.data = {};
 	$scope.movieData = function() {
-		var data = movieService.getMovie($scope.id);
+		var data = movieService.getMovie($scope.id, $scope.title);
 		data.then(function(data) {
-			//console.log(data)
+			console.log(data)
 			$scope.xhrData = data;
 		})
 		//console.log(data)
 	};
-	$scope.movieData($scope.id);
+	$scope.movieData();
 });
--- src/services/services.js
+++ src/services/services.js
@@ -1,9 +1,11 @@
 // service for communicate app and api
 
 movieApp.service('movieService', function($q, $http) {
-	this.getMovie = function(id) {
+	this.getMovie = function(id,title) {
 		movieID = id;
-		var url = "http://www.omdbapi.com/?i=" + movieID;
+		movieTitle = title;
+		var url = "http://mymovieapi.com/?title="+ movieTitle +"&type=json&plot=full&episode=0&limit=10&year=&yg=1&mt=none&lang=en-US&offset=&aka=simple&release=simple&business=0&tech=0"
+		//var url = "http://www.omdbapi.com/?i=" + movieID;
 		var deferred = $q.defer();
 		$http.get(url).then(function(response) {
         		deferred.resolve(response.data);
